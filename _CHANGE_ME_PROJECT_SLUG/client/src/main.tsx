36c1af8d480964b2e701048945efe5abf9388b05
Robert Alec Hovey <74937667+hoveydev@users.noreply.github.com>
2025-03-15 17:24:58 -0400

    react-router; added App to / route; added generic error view (#22)
    Co-authored-by: Robert Hovey <robert.hovey95@gmail.com>

--- _CHANGE_ME_PROJECT_SLUG/client/src/main.tsx
+++ _CHANGE_ME_PROJECT_SLUG/client/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import './index.css'
-import App from './App.tsx'
+import { router } from './router.tsx'
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
@@ -12,7 +13,7 @@ const client = new ApolloClient({
 createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   </ApolloProvider>,
 )
---
+++ _CHANGE_ME_PROJECT_SLUG/client/src/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ErrorView } from "./pages/Error/View";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorView />
+  },
+]);
