e52f991988e76d48a70a9b0e83bbd4256f8ccd72
Robert Alec Hovey <74937667+hoveydev@users.noreply.github.com>
2025-04-03 18:14:20 -0400

    added type gen script after setup; removed some todo placeholders (#25)
    Co-authored-by: Robert Hovey <robert.hovey95@gmail.com>

--- _CHANGE_ME_PROJECT_SLUG/server/src/index.ts
+++ _CHANGE_ME_PROJECT_SLUG/server/src/index.ts
@@ -4,7 +4,6 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { _CHANGE_ME_FIRST_API } from './datasources/_CHANGE_ME_FIRST_API';
 import resolvers from './resolvers';
 import typeDefs from './schemas';
-// TODO: Import correct classname
 
 const startApolloServer = async () => {
   const server = new ApolloServer({ typeDefs, resolvers });
@@ -13,7 +12,6 @@ const startApolloServer = async () => {
       const { cache } = server;
       return {
         dataSources: {
-          // TODO: Be sure to run `npm generate` before changing below
           _CHANGE_ME_FIRST_API_DATASOURCE: new _CHANGE_ME_FIRST_API({ cache })
         }
       };
