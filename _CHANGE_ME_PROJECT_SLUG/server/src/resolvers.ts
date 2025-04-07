59594e99637e2b9f90cc9fa37af271f090b86884
Robert Hovey <robert.hovey95@gmail.com>
2025-04-07 15:14:05 -0400

    folder structure docs; removed some comments

--- _CHANGE_ME_PROJECT_SLUG/server/src/resolvers.ts
+++ _CHANGE_ME_PROJECT_SLUG/server/src/resolvers.ts
@@ -3,7 +3,6 @@ import { Resolvers } from './types';
 export const resolvers: Resolvers = {
   Query: {
     type: (_, __, { dataSources }) => {
-      // TODO: Be sure to run `npm generate` before changing below
       return dataSources._CHANGE_ME_FIRST_API_DATASOURCE.getData();
     }
   }
